818da68745c2714fde4df6ab9942ed8c89c133eb
Eric Harvey <Eric.Harvey@ibm.com>
2024-11-05 01:31:28 -0500

    connects email submit

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
+  env: {
+    EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+    EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  },
 };
 
 export default nextConfig;
